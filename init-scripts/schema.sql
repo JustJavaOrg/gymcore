2797130565b48dd56311ba3c6bd17f870ffd8ce8
ArmanRuhit <armanruhit09@gmail.com>
2025-04-27 04:08:22 +0600

    keycload configured

--- init-scripts/schema.sql
+++ init-scripts/schema.sql
@@ -9,11 +9,8 @@ GRANT USAGE ON SCHEMA gym_app TO admin;
 GRANT CREATE ON SCHEMA gym_app TO admin;
 GRANT ALL PRIVILEGES ON DATABASE gymdb TO admin;
 
--- Create the 'keycloak' user with password
-CREATE USER keycloak WITH PASSWORD 'admin';
-
 -- Create the 'keycloak' database
 CREATE DATABASE keycloak;
 
--- Grant all privileges on the database to the 'keycloak' user
-GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
+-- Grant all privileges on the database to the 'admin' user
+GRANT ALL PRIVILEGES ON DATABASE keycloak TO admin;
