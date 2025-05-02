8140b54125405649cf6b20ef4a4455e370ee46e3
ArmanRuhit <armanruhit09@gmail.com>
2025-05-03 00:10:28 +0600

    Update V3__Update_contstains_bookings.sql

--- src/main/resources/db/migration/V3__Update_contstains_bookings.sql
+++ src/main/resources/db/migration/V3__Update_contstains_bookings.sql
@@ -1,15 +1,9 @@
-BEGIN;
-DO $$
-BEGIN
-    IF EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'fk_booking_user') THEN
-         ALTER TABLE bookings
-             DROP CONSTRAINT fk_booking_user;
-    END IF;
-END $$;
+-- Standard SQL syntax that works with both PostgreSQL and H2
+ALTER TABLE bookings
+    DROP CONSTRAINT IF EXISTS fk_booking_user;
 
 ALTER TABLE bookings
     ADD CONSTRAINT fk_booking_user
         FOREIGN KEY (user_id)
             REFERENCES users(id)
             ON DELETE CASCADE;
-COMMIT;
