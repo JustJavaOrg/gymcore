6fe57511c53e571977581243387554d52a403128
ArmanRuhit <armanruhit09@gmail.com>
2025-05-02 23:44:57 +0600

    Update V3__Update_contstains_bookings.sql

--- src/main/resources/db/migration/V3__Update_contstains_bookings.sql
+++ src/main/resources/db/migration/V3__Update_contstains_bookings.sql
@@ -1,8 +1,15 @@
-ALTER TABLE bookings
-    DROP CONSTRAINT fk_booking_user;
+BEGIN;
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'fk_booking_user') THEN
+         ALTER TABLE bookings
+             DROP CONSTRAINT fk_booking_user;
+    END IF;
+END $$;
 
 ALTER TABLE bookings
     ADD CONSTRAINT fk_booking_user
         FOREIGN KEY (user_id)
             REFERENCES users(id)
             ON DELETE CASCADE;
+COMMIT;
